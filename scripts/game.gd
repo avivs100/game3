406ef404eb6684ebb8dcdceede59367c7de1a46d
tech <tech@D-2-DANISH1-WN.rf.local>
2024-06-05 11:04:25 +0300

    adjusted laser angle

--- scripts/game.gd
+++ scripts/game.gd
@@ -64,11 +64,13 @@ func _process(delta):
 	#if pb.scroll_offset.y >= 960:
 		#pb.scroll_offset.y = 0
 
-func _on_player_laser_shot(laser_scene, location):
+func _on_player_laser_shot(laser_scene, location, muzzle_rotation_degrees):
 	var laser = laser_scene.instantiate()
+	laser.rotation_angle = muzzle_rotation_degrees
 	laser.global_position = location
 	laser_container.add_child(laser)
 	laser_sound.play()
+
 
 func _on_enemy_spawn_timer_timeout():
 	var e = enemy_scenes.pick_random().instantiate()
